fd1ea0ad1e22cf237857428cdea26ddc284cd2e3
bjudas03 <ben_judas@hotmail.com>
2017-10-05 13:58:48 -0700

    query string routed to show.ejs, working on getting form data

--- controllers/madlib.js
+++ controllers/madlib.js
@@ -27,42 +27,65 @@ router.get('/:id', function(req,res) {
 });
 
 
-//create new madlib
+//create new madlib -returns a string w/ variables
+// router.post('/', function(req, res) {
+// 	var url = 'http://libberfy.herokuapp.com';
+// 	var q = req.body.body;
+// 	request({
+// 		url: url,
+// 		qs: {q: q},
+// 		json:true
+// 	}, function(error, response, body) {
+// 		var dataObj = body;
+// 		responseText = dataObj.madlib
+// 		responseText = JSON.stringify(responseText);
+// 		// //code below works! just need to uncomment to reinitialize
+// 		db.madlib.create({
+// 			title: req.body.title,
+// 			body: responseText  //find a way to send the RESPONSE from the api call to the DB - This is sending the body (before it is jumbled)
+// 		}).then(function(result) {
+// 			console.log(result);
+// 			renderObj = {title:req.body.title, body:responseText};
+// 			console.log(renderObj);
+// 			res.render('madlibs/show', {madlib: renderObj});
+// 		});
+// 	});
+	
+// });
+ // http://libberfy.herokuapp.com?q=Hello%20world.%20Testing%20this%20API.
+//get a new madlib - returns a form as a string
 router.post('/', function(req, res) {
 	var url = 'http://libberfy.herokuapp.com';
 	var q = req.body.body;
-	// var fullUrl = url +'html_form=1&'+q;
-
+	var fullUrl = url +'?blanks=10&html_form=1&q='+q;
+	console.log(fullUrl);
 	request({
-		// url: fullUrl,
 		url: url,
-		qs: {q: q},
-		// html_form: true,
-		json:true
+		q: q,
+		html_form: true,
+		blanks: 10
+		// json: true
 	}, function(error, response, body) {
-		// console.log(body);
-		var dataObj = body;
-		responseText = dataObj.madlib
-		// console.log(responseText);
-		responseText = JSON.stringify(responseText);
-		// console.log(responseText);
+		console.log(body);
+// 		// var dataObj = body;
+// 		// responseText = dataObj.madlib
+// 		// // console.log(responseText);
+// 		// responseText = JSON.stringify(responseText);
+// 		// // console.log(responseText);
 
-		//code below works! just need to uncomment to reinitialize
-		db.madlib.create({
-			title: req.body.title,
-			body: responseText  //find a way to send the RESPONSE from the api call to the DB - This is sending the body (before it is jumbled)
-		}).then(function(result) {
-			console.log(result);
-			res.render("madlibs/show", {madlib: responseText});
-		// }); //this is in for testing. Remove
-		// // .then(function(madlib) {
-		// 	// res.redirect('/madlibs/')
+// 		// //code below works! just need to uncomment to reinitialize
+// 		// db.madlib.create({
+// 		// 	title: req.body.title,
+// 		// 	body: responseText  //find a way to send the RESPONSE from the api call to the DB - This is sending the body (before it is jumbled)
+// 		// }).then(function(result) {
+// 		// 	console.log(result);
+// 		// 	res.render("madlibs/show", {madlib: responseText});
+// 		// }); //this is in for testing. Remove
+// 		// // .then(function(madlib) {
+// 		// 	// res.redirect('/madlibs/')
 		});
 	});
-	
-});
-
-
+// // });	
 
 
 
